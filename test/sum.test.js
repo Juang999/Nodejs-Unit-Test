5f67d7da13b01a8dcde8411bf0f07fc6e601fc19
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-08-25 06:18:47 +0700

    second learning

--- test/sum.test.js
+++ test/sum.test.js
@@ -5,3 +5,15 @@ test("first test to sum", () => {
 
     expect(total).toBe(3)
 })
+
+test("second test to sum", () => {
+    const total = sum(1, 2)
+
+    expect(total).toBe(3)
+})
+
+test("third test to sum", () => {
+    const total = sum(1, 2)
+
+    expect(total).toBe(3)
+})
